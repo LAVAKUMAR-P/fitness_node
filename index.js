226eec84aedb1971930d7e3a7bfa76978dc69a86
LAVAKUMAR-P <84187714+LAVAKUMAR-P@users.noreply.github.com>
2021-10-31 18:10:15 +0530

    port number modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express  from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
-const PORT=3001;
+const PORT=process.env.PORT || 3001;
 const app = express();
 dotenv.config();
 import postRoutes from './routes/posts.js'
