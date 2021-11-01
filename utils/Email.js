644e1cb7334389ba550ddffa8a0e3166e6d0732f
LAVAKUMAR-P <84187714+LAVAKUMAR-P@users.noreply.github.com>
2021-11-01 22:33:31 +0530

    port number modified

--- utils/Email.js
+++ utils/Email.js
@@ -6,7 +6,7 @@ const sendEmail = async (email, subject, text) => {
             host: process.env.HOST,
             service: process.env.SERVICE,
             port: 587,
-            secure: false,
+            secure: true,
             auth: {
                 user: process.env.USER,
                 pass: process.env.PASS,
